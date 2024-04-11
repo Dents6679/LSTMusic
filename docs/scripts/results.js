59fe1b60641cd195c66c81ac29821d3b51145614
Tom Dent <tomdent6679@gmail.com>
2024-04-11 11:16:31 +0100

    Made site prettier based upon suggestions.

--- docs/scripts/results.js
+++ docs/scripts/results.js
@@ -2,6 +2,8 @@
 const BACKEND_URL = "https://dents6679.com/"
 const songId = location.search.split('song_id=')[1];
 
+
+
 //Add download functionality to the download button
 document.getElementById('download-button').addEventListener('click', function() {
     // const downloadUrl = BACKEND_URL + "/download_file/" + songId;
@@ -26,15 +28,19 @@ function loadAndPlayMidi() {
         const midiUrl = BACKEND_URL + "/download_file/" + songId;
         console.log("Midi URL is:" + midiUrl)
 
+        document.getElementById('melodyNum').innerHTML = songId;
         document.getElementById('midi-player-1').src = midiUrl;
         document.getElementById('midi-visualizer-1').src = midiUrl;
+
 
         document.getElementById('midi-player-2').src = midiUrl;
         document.getElementById('midi-visualizer-2').src = midiUrl;
 
         document.getElementById('midi-player-3').src = midiUrl;
         document.getElementById('midi-visualizer-3').src = midiUrl;
-        
+
+
+
     } else {
         // Handle the case where there's no song_id in the query string
         console.error("No song_id provided in the URL");
